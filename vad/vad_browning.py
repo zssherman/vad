1b26660f84d1abc0f2f0363eeae72386c1b4616d
Zachary Sherman <zsherman@anl.gov>
2017-04-12 09:59:49 -0500

    MAINT: Added gatefilter, height and weight arguments. The gatefilter argument allows for the filtering of velocity gate. The height argument allows the user to give their own wanted sampling heights, and weight argument for how the values are weighting when interpolating.

--- vad/vad_browning.py
+++ vad/vad_browning.py
@@ -12,27 +12,27 @@ Retrieval of VADs from a radar object.
     :template: dev_template.rst
 
     velocity_azimuth_display
+    _horizontal_winds
     _inverse_dist_squared
     _Average1D
 
 """
 
 import numpy as np
-import numpy.ma as ma
 
 from pyart.core import HorizontalWindProfile
-from scipy.interpolate import interp1d
 
 
-def velocity_azimuth_display(radar, velocity=None,
-                             z_want=None, valid_ray_min=16,
-                             gatefilter=False, window=2):
+def velocity_azimuth_display(
+        radar, velocity=None, height=None,
+        valid_ray_min=16, gatefilter=None, window=2,
+        weight='equal'):
     """
     Velocity azimuth display.
-    
+
     Note: If a specific sweep is desired, before using the
-    velocity_azimuth_display function, use:
-    radar = radar.extract_sweeps([0])
+    velocity_azimuth_display function, use, for example:
+    one_sweep_radar = radar.extract_sweeps([0])
 
     Parameters
     ----------
@@ -44,22 +44,23 @@ def velocity_azimuth_display(radar, velocity=None,
 
     Other Parameters
     ----------------
-    z_want : array
-        Height array user would like to use for VAD
-        calculation. None will result in a z_want of
-        np.linspace and use of _inverse_dist_squared
-        and _Average1D functions. Note, height must have
-        same shape as expected u_wind and v_wind if user
-        provides z_want.
+    height : array
+        Array of desired heights to be sampled for the vad
+        calculation.
     valid_ray_min : int
         Amount of rays required to include that level in
         the VAD calculation.
     gatefilter : GateFilter
-        Used to correct the velocity field before its use
-        in the VAD calculation.
+        A GateFilter indicating radar gates that should be excluded when
+        from the import vad calculation.
     window : int
-        Value to use for window calculation in _Averag1D
-        function.
+        Value to use for window when determing new values in the
+        _Averag1D function.
+    weight : string
+        A string to indicate weighting method to use. 'equal' for
+        equal weighting when interpolating or 'idw' for inverse
+        distribution squared weighting for interpolating.
+        Default is 'equal'.
 
     Returns
     -------
@@ -77,70 +78,90 @@ def velocity_azimuth_display(radar, velocity=None,
 
     Reference
     ----------
-    K. A. Browning and R. Wexler, 1968: The Determination 
-    of Kinematic Properties of a Wind Field Using Doppler 
+    K. A. Browning and R. Wexler, 1968: The Determination
+    of Kinematic Properties of a Wind Field Using Doppler
     Radar. J. Appl. Meteor., 7, 105–113
 
     """
 
-    if gatefilter:
-        gatefilter = pyart.correct.GateFilter(radar)
-        corr_vel_region = pyart.correct.dealias_region_based(
-        radar, vel_field=velocity, keep_original=False, 
-        gatefilter = None, nyquist_vel=None, centered = True)
-
-        velocities = corr_vel_region['data'][:, :]
-
+    if velocity is None:
+        velocity_used = 'velocity'
     else:
-        if velocity is None:
-            velocity_used = 'velocity'
-        else:
-            velocity_used = velocity
+        velocity_used = velocity
 
-        velocities = radar.fields['velocity']['data'][:, :]
-
+    velocities = radar.fields[velocity_used]['data']
+    #mask = velocities.mask
+    #velocities[np.where(mask)] = np.nan
+    if gatefilter is not None:
+        velocities = np.ma.masked_where(
+            gatefilter.gate_excluded, velocities)
     azimuths = radar.azimuth['data'][:]
     elevation = radar.fixed_angle['data'][0]
 
-    u_wind, v_wind = _vad_calculation(velocities, azimuths,
-                                      elevation, valid_ray_min)
+    u_wind, v_wind = _horizontal_winds(velocities, azimuths,
+                                       elevation, valid_ray_min)
+    bad = np.logical_or(np.isnan(u_wind), np.isnan(v_wind))
+    good_u_wind = u_wind[~bad]
+    good_v_wind = v_wind[~bad]
 
-    if z_want is None:
-        height_used = radar.gate_z['data'][0]
+    radar_height = radar.gate_z['data'][0]
+    good_height = radar_height[~bad]
 
-        bad = np.logical_or(np.isnan(u_wind), np.isnan(v_wind))
-        good_u_wind = u_wind[~bad]
-        good_v_wind = v_wind[~bad]
-        good_height = height_used[~bad]
+    if height is None:
+        z_want = np.linspace(0, 1000, 100)[:50]
+    else:
+        z_want = height
 
-        z_want = np.linspace(np.min(good_height),
-                             np.max(good_height), 100)
-
+    try:
         print('max height', np.max(good_height), ' meters')
         print('min height', np.min(good_height), ' meters')
+    except ValueError:
+        raise ValueError('Not enough data in this radar sweep ' \
+                         'for a vad calculation.')
 
-        u_interp = Average1D(good_height, good_u_wind,
-                             z_want[1] - z_want[0] / window)
-        v_interp = Average1D(good_height, good_v_wind,
-                             z_want[1] - z_want[0] / window)
+    u_interp = _Average1D(good_height, good_u_wind,
+                          z_want[1] - z_want[0] / window, weight)
+    v_interp = _Average1D(good_height, good_v_wind,
+                          z_want[1] - z_want[0] / window, weight)
 
-        u_wanted = u_interp(z_want)
-        v_wanted = v_interp(z_want)
-        u_wanted = np.ma.masked_equal(u_wanted, 99999.)
-        v_wanted = np.ma.masked_equal(v_wanted, 99999.)
+    u_wanted = u_interp(z_want)
+    v_wanted = v_interp(z_want)
+    u_wanted = np.ma.masked_equal(u_wanted, 99999.)
+    v_wanted = np.ma.masked_equal(v_wanted, 99999.)
 
-        vad = HorizontalWindProfile.from_u_and_v(z_want, u_wanted,
-                                                 v_wanted)
-    else:
-        vad = HorizontalWindProfile.from_u_and_v(z_want, u_wind,
-                                                 v_wind)
+    vad = HorizontalWindProfile.from_u_and_v(
+        z_want, u_wanted, v_wanted)
     return vad
 
-def _vad_calculation(velocities, azimuths,
-                     elevation, valid_ray_min):
+def _horizontal_winds(velocities, azimuths,
+                      elevation, valid_ray_min):
     """ Calculates VAD for a scan and returns u_mean and
-    v_mean. velocities is a 2D array, azimuths is a 1D array,
-    elevation is a number. """
+    v_mean. velocities is a 2D array, azimuths is a 1D
+    array, elevation is a number.
+
+    Note:
+    We need to solve: Ax = b
+    where:
+    A = [sum_sin_squared_az, sum_sin_cos_az    ] = [a, b]
+        [sum_sin_cos_az,     sum_cos_squared_az]   [c, d]
+    b = [sum_sin_vel_dev] = [b_1]
+        [sum_cos_vel_dev]   [b_2]
+    The solution to this is:
+    x = A-1 * b
+
+    A-1 is:
+     1    [ d,  -b ]
+    --- * [ -c,  a ]
+    |A|
+
+    and the determinate, det is: det = a*d - b*c
+
+    Therefore the elements of x are:
+
+    x_1 = (d* b_1  + -b * b_2) / det = (d*b_1 - b*b_2) / det
+    x_2 = (-c * b_1 +  a * b_2) / det = (a*b_2 - c*b_1) / det
+
+    """
     velocities = velocities.filled(np.nan)
     shape = velocities.shape
     _, nbins = velocities.shape
@@ -157,14 +178,11 @@ def _vad_calculation(velocities, azimuths,
     sin_az[invalid] = np.nan
     cos_az[invalid] = np.nan
 
-    mean_velocity_per_gate = np.nanmean(velocities,
-                                        axis=0).reshape(1, -1)
+    mean_velocity_per_gate = np.nanmean(velocities, axis=0).reshape(1, -1)
     velocity_deviation = velocities - mean_velocity_per_gate
 
-    sum_cos_vel_dev = np.nansum(cos_az * velocity_deviation,
-                                axis=0)
-    sum_sin_vel_dev = np.nansum(sin_az * velocity_deviation,
-                                axis=0)
+    sum_cos_vel_dev = np.nansum(cos_az * velocity_deviation, axis=0)
+    sum_sin_vel_dev = np.nansum(sin_az * velocity_deviation, axis=0)
 
     sum_sin_cos_az = np.nansum(sin_az * cos_az, axis=0)
     sum_sin_squared_az = np.nansum(sin_az**2, axis=0)
@@ -192,16 +210,19 @@ def _vad_calculation(velocities, azimuths,
     return u_mean, v_mean
 
 def _inverse_dist_squared(dist):
+    """ Obtaining distance weights by using distance weighting
+    interpolation, using the inverse distance-squared relationship.
+    """
     weights = 1 / (dist * dist)
     weights[np.isnan(weights)] = 99999.
     return weights
 
 
 class _Average1D(object):
-    """ Used to obtain a weighted average for height
-    and desired heights."""
+    """ Used to find the nearest gate height and horizontal wind
+    value with respect to the user's desired height. """
 
-    def __init__(self, x, y, window, weight='equal',
+    def __init__(self, x, y, window, weight,
                  fill_value=99999.):
 
         sort_idx = np.argsort(x)
@@ -238,6 +259,5 @@ class _Average1D(object):
             else:
                 distances = x_in_window - center
                 weights = self.weight_func(distances)
-                y_new[i] = np.average(y_in_window,
-                                      weights=weights)
+                y_new[i] = np.average(y_in_window, weights=weights)
         return y_new
